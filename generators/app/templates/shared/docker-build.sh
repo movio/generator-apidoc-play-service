0752ee3699b0161445a9aa87b1c8a85052f78e8c
jackhopner <jack@movio.co>
2016-01-08 15:02:13 +1300

    Add docker files to template

---
+++ generators/app/templates/shared/docker-build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+SERVICE_NAME=<%= props.appName %>
+TAG=$1
+
+if [ -z "$TAG" ] ; then
+  echo "Tag not specified - eg 0.1.0"
+  echo "Note: you need to have the dist zip present"
+  echo
+  echo "Usage:"
+  echo "./docker-build.sh <tag>"
+  echo
+  echo "Example:"
+  echo "./docker-build.sh latest"
+  exit
+fi
+
+rm -rf "$SERVICE_NAME"
+
+unzip "$SERVICE_NAME".zip
+
+docker build -t "docker.movio.co/$SERVICE_NAME:$TAG" .
