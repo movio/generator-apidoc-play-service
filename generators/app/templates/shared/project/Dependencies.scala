6beea27c866cc90e2a3235ce971a38748f8934e7
jackhopner <jack@movio.co>
2016-07-28 13:18:43 +1200

    Fix logging config

--- generators/app/templates/shared/project/Dependencies.scala
+++ generators/app/templates/shared/project/Dependencies.scala
@@ -2,7 +2,11 @@ import sbt._
 
 object Dependencies {
   val apidocDependencies = Set(
-    "<%= props.apidocOrg %>" %% "<%= props.apidocApp %>-kafkalib_0_8" % "<%= props.apidocVersion %>",
-    "<%= props.apidocOrg %>" %% "<%= props.apidocApp %>-playlib" % "<%= props.apidocVersion %>"
+    "<%= props.apidocOrg %>" %% "<%= props.apidocApp %>-kafkalib_0_8" % "<%= props.apidocVersion %>" excludeAll (
+      ExclusionRule("org.slf4j", "slf4j-log4j12")
+    ),
+    "<%= props.apidocOrg %>" %% "<%= props.apidocApp %>-playlib" % "<%= props.apidocVersion %>" excludeAll (
+      ExclusionRule("org.slf4j", "slf4j-log4j12")
+    ),
   )
 }
